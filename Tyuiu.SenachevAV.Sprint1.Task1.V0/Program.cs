6c0fc46feb43635f5efd47a907c8061921d9b526
Андрей Сеначев <andrey30-04-2005god@yandex.ru>
2024-09-14 01:47:53 +0500

    Task1

---
+++ Tyuiu.SenachevAV.Sprint1.Task1.V0/Program.cs
@@ -0,0 +1,11 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        string name = "rain";
+        int time = 77;
+        Console.WriteLine(name + " every day " + time + " puddles");
+        Console.ReadLine();
+    }
+}
